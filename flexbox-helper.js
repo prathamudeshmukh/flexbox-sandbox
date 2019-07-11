10e2dc35ba3f1735b4f705ae0b982320a3d5e731
Administrator <prathamudeshmukh@gmail.com>
2019-07-11 22:45:58 +0530

    Added material color to the tiles

--- flexbox-helper.js
+++ flexbox-helper.js
@@ -1,5 +1,4 @@
 var defaultchildrenClass = 'default-children';
-var baseUrl = 'http://192.168.0.4:8989/';
 var childrenCnt = 1;
 var currentParent = null;
 
@@ -11,7 +10,6 @@ initializeBuilder = () => {
 }
 
 onChildClick = (event) => {
-    console.log('onchildclick',event.target.id);
     currentParent.removeClass('selected-child');
     currentParent = $(event.target);
     currentParent.addClass('selected-child');
@@ -20,6 +18,7 @@ onChildClick = (event) => {
 getDefaultChild = () => {
     var defaultChild = $('<div id="child-'+childrenCnt+'" data-ischild="true">'+childrenCnt+'</div>');
     defaultChild.attr('class', defaultchildrenClass);
+    defaultChild.css('backgroundColor', materialColor());
     defaultChild.click(onChildClick);
     childrenCnt ++;
     return defaultChild;
@@ -32,47 +31,45 @@ initializeControls = () => {
     $("input[name='align-items']").click(onAlignItemsClick);
     $("input[name='align-content']").click(onAlignContentClick);
     $("#add-child").click(onAddChild);
+    $("#remove-child").click(onRemoveChild);
 }
 
 onAddChild = () => {
-    console.log('onaddchild');
     var defaultChild = getDefaultChild();
     var isNewlyAddedChild = currentParent.data('ischild');
     if (isNewlyAddedChild && currentParent.children().length === 0) {
-        console.log('childrenSize:',currentParent.children().length);
-        // console.log('childrenText:',currentParent.text());
-        // console.log('childrenHTML:',currentParent.html());
         currentParent.text('');
     }
     currentParent.append(defaultChild);
 }
 
+onRemoveChild = () => {
+    var newParent = currentParent.parent();
+    currentParent.remove();
+    currentParent = newParent;
+}
+
 onFlexDirectionClick = (event) => {
-    console.log('Flex Direction',event.target.value);
     var flexDirection = event.target.value;
     currentParent.css('flex-direction', flexDirection)
 }
 
 onFlexWrapClick = (event) => {
-    console.log('Flex Wrap',event.target.value);
     var flexWrap = event.target.value;
     currentParent.css('flex-wrap', flexWrap)
 }
 
 onJustifyContentClick = (event) => {
-    console.log('Justify Content',event.target.value);
     var justifyContent = event.target.value;
     currentParent.css('justify-content', justifyContent)
 }
 
 onAlignItemsClick = (event) => {
-    console.log('Align Items',event.target.value);
     var alignItems = event.target.value;
     currentParent.css('align-items', alignItems)
 }
 
 onAlignContentClick = (event) => {
-    console.log('Align Content',event.target.value);
     var alignContent = event.target.value;
     currentParent.css('align-content', alignContent)
 }
